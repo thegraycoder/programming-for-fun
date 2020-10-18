6134c3fbdf80b842e3d71f81ca7416144e3b7486
Kshitij Saxena <kshitj.saxena@mecuris.com>
2020-10-18 15:31:45 +0200

    Improving performance

--- python/medium/3sum.py
+++ python/medium/3sum.py
@@ -4,24 +4,30 @@
 
 
 def three_sum(nums):
-    sums = set()
+    sums = []
     nums.sort()
     for i in range(len(nums) - 2):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
         left = i + 1
         right = len(nums) - 1
 
         while left < right:
             if nums[i] + nums[left] + nums[right] == 0:
-                sums.add((nums[i], nums[left], nums[right]))
-                left += 1
+                sums.append([nums[i], nums[left], nums[right]])
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
                 right -= 1
+                left += 1
             elif nums[i] + nums[left] + nums[right] < 0:
                 # We need bigger numbers
                 left += 1
             else:
                 # We need smaller numbers
                 right -= 1
-    return list(sums)
+    return sums
 
 
 # Test cases
